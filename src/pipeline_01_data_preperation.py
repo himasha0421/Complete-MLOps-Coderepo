6d02cae98253d35e863219a84fed0712eb2e0f87
himasha0421 <himasha0421@gmail.com>
2023-03-07 01:04:30 +0530

    add config reader in main

--- src/pipeline_01_data_preperation.py
+++ src/pipeline_01_data_preperation.py
@@ -4,6 +4,22 @@ import os
 
 import yaml
 
+
+def read_params(config_path):
+    # read the config path
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+        print("Config Info : \n",config)
+    return config
+
+def main(config_path, datasource):
+    """main operation function"""
+
+    # step1 . read config file
+    config_meta =  read_params(config_path=config_path)
+
+    return None
+
 if __name__=='__main__':
     args =  argparse.ArgumentParser()
     # add arguments
@@ -11,3 +27,6 @@ if __name__=='__main__':
     args.add_argument("--datasource", default='data/raw/wafer-dataset-main')
 
     parsed_args =  args.parse_args()
+
+    # call the main function
+    main(config=parsed_args.config, datasource=parsed_args.datasource )
